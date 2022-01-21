8c6310563e72a30cf55381fe4c09ab5712da1494
Morgan Jacquet <jacquetmorganmj@gmail.com>
2022-01-21 09:32:23 +0100

    wip change listing

--- index.php
+++ index.php
@@ -16,27 +16,94 @@ if (isset($_POST['dl_button'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listing du dossier</title>
+    <title>Liste de dossiers et fichiers "/<?=basename(getcwd())?>"</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap.min.css" integrity="sha512-BMbq2It2D3J17/C7aRklzOODG1IQ3+MHw3ifzBHMBwGO/0yUqYmsStgBjI0z5EYlaDEFnvYV7gNYdD3vFLRKsA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" />
+    <style>
+        .sorting_disabled {
+            background : none;
+        }
+    </style>
 </head>
 <body>
-<ul>
-    <?php
-    //je boucle sur les fichier des dossier et sous dossier de mon dossier courant
-    foreach (list_file(getcwd()) as $value) {
-        //je reformate ma value pour avoir uniquement le chemin partant du dossier courant et pas de la base de ma machine
-        $value = str_replace(getcwd()."/", "", $value);
-    ?>
-    <li>
-        <form method="POST">
-            <?=$value;?> <a href="<?=$value;?>">Voir le fichier</a>
-            <input type="hidden" name="dl_file" value="<?=$value;?>">
-            <input type="submit" name="dl_button" value="Télécharger">
-        </form>
-    </li>
-    <?php
-    unset($value);
-    }
-    ?>
-</ul>
+    <div class="container">
+        <h1 class="text-center">Liste de dossiers et fichiers "/<?=basename(getcwd())?>"</h1>
+        <table id="list_files" class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th width="5%" class="text-center">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check_all">
+                            <label class="form-check-label" for="check_all"> Tous</label>
+                        </div>
+                    </th>
+                    <th width="5%">Types</th>
+                    <th>Noms</th>
+                    <th>Dossiers parent</th>
+                    <th>Chemins</th>
+                    <th class="text-center">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach (list_file(getcwd()) as $value) {
+                        $value_file = get_info_file($value);
+                ?>
+                <tr>
+                    <td class="text-center">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        </div>
+                    </td>
+                    <td class="text-center">
+                        <i class="<?=$value_file['icon'];?> fa-2x"></i>
+                        <p style="display: none;"><?=$value_file['type'];?></p>
+                    </td>
+                    <td><?=$value_file['name'];?></td>
+                    <td><?=$value_file['parent_directory'];?></td>
+                    <td><?=$value_file['path'];?></td>
+                    <td class="text-center">
+                        <a href="<?=$value_file['path'];?>" target="_blank"><i class="far fa-eye" style="font-size: 18px;"></i></a>  
+                        <?=$value_file['type'] != 'directory' ? '<a href="#"><i class="fas fa-file-download" style="font-size: 18px;"></i></a>' : '<a href="#"><i class="fas fa-file-archive" style="font-size: 18px;"></i></a>';?>
+                    </td>
+                </tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/dataTables.bootstrap.min.js" integrity="sha512-F0E+jKGaUC90odiinxkfeS3zm9uUT1/lpusNtgXboaMdA3QFMUez0pBmAeXGXtGxoGZg3bLmrkSkbK1quua4/Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js" integrity="sha512-fzff82+8pzHnwA1mQ0dzz9/E0B+ZRizq08yZfya66INZBz86qKTCt9MLU0NCNIgaMJCgeyhujhasnFUsYMsi0Q==" crossorigin="anonymous"></script>
+<script>
+$(document).ready( function () {
+    $('#list_files').DataTable({
+        "order": [],
+        "columnDefs": [
+            {
+                "targets": 0,
+                "orderable": false
+            },
+            {
+                "targets": 5,
+                "orderable": false
+            }
+        ],
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/fr_fr.json'
+        },
+        "pageLength": 200,
+        "paging": false
+    });
+
+    $("#check_all").click(function () {
+        $('input:checkbox').not(this).prop('checked', this.checked);
+    });
+});
+</script>
 </html>
--- listing.php
+++ listing.php
@@ -29,6 +29,8 @@ function list_file($ori_folder){
     //on supprime . et ..
     unset($folder[array_search('.', $folder, true)]);
     unset($folder[array_search('..', $folder, true)]);
+    unset($folder[array_search('.DS_Store', $folder, true)]);
+
 
     //si le dossier est vide on retourne rien
     if (count($folder) < 1){
@@ -39,15 +41,116 @@ function list_file($ori_folder){
     foreach($folder as $folder_value){
         if(is_dir($ori_folder.'/'.$folder_value)) {
             //si on detecte un sous dossier on le repasse dans notre fonction list_file
-            $folder_result = list_file($ori_folder.'/'.$folder_value);
+            $result[] = $ori_folder.'/'.$folder_value;
+            $result[] = list_file($ori_folder.'/'.$folder_value);
         } else {
             //si c'est juste un fichier on le reformate avec le chemin exact
-            $folder_result = $ori_folder.'/'.$folder_value;
+            $result[] = $ori_folder.'/'.$folder_value;
         }
-        //on stock dans un tableau chaque lien de fichier
-        $result[] = $folder_result;
     }
 
     //je retourne a plat mon tableau de chemin de fichier pour faciliter l'affichage
     return array_flatten($result);
+}
+
+function get_info_file($path_file){
+    if (is_dir($path_file)) {
+        $result = array(
+            'type' => 'directory',
+            'path' => str_replace(getcwd()."/", "", $path_file),
+            'icon' => get_icon_file('directory'),
+            'parent_directory' => str_replace(getcwd()."/", "", dirname($path_file)),
+            'name' => basename($path_file)
+        );
+    } else {
+        $result = array(
+            'type' => mime_content_type($path_file),
+            'path' => str_replace(getcwd()."/", "", $path_file),
+            'parent_directory' => str_replace(getcwd()."/", "", dirname($path_file)),
+            'icon' => get_icon_file(mime_content_type($path_file)),
+            'name' => basename($path_file)
+        );
+    }
+
+    if ($result['parent_directory'] == getcwd()) {
+        $result['parent_directory'] = '-';
+    }
+
+    return $result;
+}
+
+function get_icon_file($type_file)
+{
+    switch ($type_file) {
+        case 'text/html':
+            return 'fab fa-html5';
+            break;
+
+        case 'text/x-php':
+            return 'fas fa-file-code';
+            break;
+
+        case 'image/jpeg':
+            return 'fas fa-image';
+            break;
+
+        case 'image/png':
+            return 'fas fa-image';
+            break;
+        
+        case 'video/mp4':
+            return 'fas fa-film';
+            break;
+
+        case 'text/plain':
+            return 'fas fa-file-alt';
+            break;
+
+        case 'text/x-shellscript':
+            return 'fas fa-terminal';
+            break;
+        
+        case 'application/pdf':
+            return 'fas fa-file-pdf';
+            break;
+        
+        case 'application/octet-stream':
+            return 'fas fa-file';
+            break;
+
+        case 'text/x-perl':
+            return 'fas fa-scroll';
+            break;
+        
+        case 'image/svg+xml':
+            return 'fas fa-bezier-curve';
+            break;
+        
+        case 'directory':
+            return 'fas fa-folder';
+            break;
+        
+        case 'application/msword':
+            return 'fas fa-file-word';
+            break;
+        
+        case 'application/zip':
+            return 'fas fa-file-archive';
+            break;
+        
+        case 'audio/x-m4a':
+            return 'fas fa-music';
+            break;
+        
+        case 'video/quicktime':
+            return 'fas fa-film';
+            break;
+        
+        case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheetapplication/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+            return 'fas fa-file-excel';
+            break;
+        
+        default:
+            return 'fas fa-question-circle';
+    }
 }
